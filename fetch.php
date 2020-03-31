a6c17498460faa11fc301a9b04e45fa1ee2b8a58
saldisucipto <saldisucipto@gmail.com>
2020-03-31 21:25:21 +0700

    intial commit

--- fetch.php
+++ fetch.php
@@ -12,7 +12,7 @@ $output = '
 <table class="table table-striped table-bordered">
     <tr>
         <th>First Name</th>
-        <th>Last Name</th>
+        <th>Last Name</th>  
         <th>Edit</th>
         <th>Delete</th>
     </tr>
